c22dca5e944aec72725b5346079e4bb5eb2c9c82
bee98 <43488353+bee98@users.noreply.github.com>
2020-03-17 16:31:20 +0700

    Done

--- mondai6.js
+++ mondai6.js
@@ -7,9 +7,7 @@ let arr = [];
 let sum = 0;
 arr = process.argv.slice(2);
 let arrN = arr.map((element)=>Number(element));
-arrN.forEach((element)=>{sum += element;})
-const value  = sum/arrN.length;
-if(value - Math.floor(value) > 0.5)
-console.log(Math.floor(value) + 1);
-else 
+const max = Math.max(...arrN);
+const min = Math.min(...arrN);
+const value  = (min+max)/2;
 console.log(Math.floor(value));
